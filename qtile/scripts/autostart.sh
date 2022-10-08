ba0246dd5a7e9d545c894e92442b08101f516aef
trien1 <abdalrahmanaldkak@gmail.com>
2022-10-08 19:09:36 +0200

    changes

--- qtile/scripts/autostart.sh
+++ qtile/scripts/autostart.sh
@@ -49,7 +49,7 @@ run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
 run xfce4-power-manager &
 #numlockx on &
 #blueberry-tray &
-#picom --config $HOME/.config/picom/picom.conf &
+picom --config $HOME/.config/picom/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 
